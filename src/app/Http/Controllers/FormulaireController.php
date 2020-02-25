1f8a744cd91c07f8597939f68d9e0b66927f9ae7
Sharkerz <292691@supinfo.com>
2020-02-25 14:27:02 +0100

    fixs

---
+++ src/app/Http/Controllers/FormulaireController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\formulaire;
+use Illuminate\Http\Request;
+
+class FormulaireController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\formulaire  $formulaire
+     * @return \Illuminate\Http\Response
+     */
+    public function show(formulaire $formulaire)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\formulaire  $formulaire
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(formulaire $formulaire)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\formulaire  $formulaire
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, formulaire $formulaire)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\formulaire  $formulaire
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(formulaire $formulaire)
+    {
+        //
+    }
+}
