231b88c444487b7260aa4964239488796beb66f7
Bouwilal <301228@supinfo.com>
2020-03-02 10:38:38 +0100

    Formualaire view

--- src/resources/views/formulaire/create.blade.php
+++ src/resources/views/formulaire/create.blade.php
@@ -1,18 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <form method="post">
+                <label for="NomFormulaire">Nom du Formulaire:</label>
+                    <input label="Nom du Formulaire:" type="text" name="test">
+                    <input type="submit"></br>
 
-<body>
+                    <input type="text" name="test">
 
-    <form method="post">
-        <input type="text" name="test">
-        <input type="submit">
+                </form>
 
-    </form>
-
-
-
-</body>
-
+            </div>
+        </div>
+    </div>
+</div
 @endsection
 
