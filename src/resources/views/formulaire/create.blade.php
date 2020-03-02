1da4580aae48d63da3d68f8cf72555ff28da171f
Bouwilal <301228@supinfo.com>
2020-03-02 11:34:16 +0100

    Formulaire  create view

--- src/resources/views/formulaire/create.blade.php
+++ src/resources/views/formulaire/create.blade.php
@@ -6,10 +6,13 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <form method="post">
-                <label for="NomFormulaire">Nom du Formulaire:</label>
-                    <input label="Nom du Formulaire:" type="text" name="test"></br>
-                    <input type="text" name="test"></br>
+                <form action="" method="get">
+                    <label for="NomFormulaire">Nom du Formulaire:</label>
+                        <input label="Nom du Formulaire:" type="text" name="test" required></br>    
+                    <label for="NomFormulaire">Début de la publication:</label>
+                    <input type="date" id="start" name="start_formulaire" value="2018-07-22" min="2018-01-01" max="2018-12-31"></br>
+                    <label for="NomFormulaire">Fin de la publication:</label>
+                    <input type="date" id="start" name="end_formulaire" value="2018-07-22" min="start_formulaire" max="2018-12-31">
                     <input type="submit">
                 </form>
 
