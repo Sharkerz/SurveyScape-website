9a0701153557ba0df2417d3e9861df5ff2296d91
Bouwilal <301228@supinfo.com>
2020-03-02 10:43:26 +0100

    Modif Formulaire

--- src/resources/views/formulaire/create.blade.php
+++ src/resources/views/formulaire/create.blade.php
@@ -1,22 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
+<h1>Création d'un formulaire</h1>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <form method="post">
                 <label for="NomFormulaire">Nom du Formulaire:</label>
-                    <input label="Nom du Formulaire:" type="text" name="test">
-                    <input type="submit"></br>
-
-                    <input type="text" name="test">
-
+                    <input label="Nom du Formulaire:" type="text" name="test"></br>
+                    <input type="text" name="test"></br>
+                    <input type="submit">
                 </form>
 
             </div>
         </div>
     </div>
-</div
+</div>
 @endsection
 
