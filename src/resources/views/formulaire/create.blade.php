50d1edc9626e6dec6214be6ffbe6a0a7aa3f85e6
Sharkerz <292691@supinfo.com>
2020-03-02 11:40:17 +0100

    fix form create

--- src/resources/views/formulaire/create.blade.php
+++ src/resources/views/formulaire/create.blade.php
@@ -8,7 +8,7 @@
             <div class="card">
                 <form action="" method="get">
                     <label for="NomFormulaire">Nom du Formulaire:</label>
-                        <input label="Nom du Formulaire:" type="text" name="test" required></br>    
+                        <input label="Nom du Formulaire:" type="text" name="name" required></br>
                     <label for="NomFormulaire">Début de la publication:</label>
                     <input type="date" id="start" name="start_formulaire" value="2018-07-22" min="2018-01-01" max="2018-12-31"></br>
                     <label for="NomFormulaire">Fin de la publication:</label>
