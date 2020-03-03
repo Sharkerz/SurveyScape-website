9d3ac226d1d7080c3af51514da6906a3aeceddcd
Sharkerz <292691@supinfo.com>
2020-03-03 13:33:09 +0100

    add api users

--- src/routes/api.php
+++ src/routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('/formulaires', 'API\FormulaireController');
+Route::apiResource('/users', 'API\UserController');
