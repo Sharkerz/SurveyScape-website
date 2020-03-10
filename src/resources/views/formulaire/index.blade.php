00e3d368b036fe481bcb20b692d294ae6687cb3c
Gandalf0u <leo.auvray@free.fr>
2020-03-10 14:12:45 +0100

    background image + index formulaire

--- src/resources/views/formulaire/index.blade.php
+++ src/resources/views/formulaire/index.blade.php
@@ -4,27 +4,34 @@
 
 <link href="{{ asset('css/index.css') }}" rel="stylesheet">
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-            <div class="card-header">
-                <h1>Formulaires</h1>
-                <button onclick="window.location.href='/formulaires/create'" id="btn-create" type="button" class="btn btn-success">Créer un formulaire</button>
-            </div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Nom</th>
-                            <th>Propriétaire</th>
-                            <th>Date de début</th>
-                            <th>Date de fin</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody id="ListFormulaire">
-                    </tbody>
-                </table>
+<div class="row justify-content-center">
+    <h1 class="titre">Vos Formulaires</h1>
+</div>
+
+<div class="div-blanche">
+    <div class="container">
+        <div class="row justify-content-center">
+            <button onclick="window.location.href='/formulaires/create'" id="btn-create" type="button" class="btn btn-success bouton-creation">Créer un formulaire</button>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Nom</th>
+                                <th>Propriétaire</th>
+                                <th>Date de début</th>
+                                <th>Date de fin</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody id="ListFormulaire">
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
