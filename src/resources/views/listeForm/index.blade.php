bf6d2c86aa6baa9b1ae5bbb580d1cfd2cd3cc54a
Maxence <maxence.lux@supinfo.com>
2020-06-08 18:17:13 +0200

    add list formulaires

--- src/resources/views/listeForm/index.blade.php
+++ src/resources/views/listeForm/index.blade.php
@@ -2,6 +2,44 @@
 
 @section('content')
 
-list forms
+<link href="{{ asset('css/list_form.css') }}" rel="stylesheet">
+
+<div class="container-fluid" id="formulaire_list">
+
+    <div id="header_list">
+        <h5 class="header_title">Formulaires public disponibles</h5>
+        <br>
+        <hr>
+    </div>
+
+    {{ $formulaires->links() }}
+
+    @foreach($formulaires as $formulaire)
+
+
+        <div class="card formulaire" id="{{ $formulaire->id }}">
+                <img class="card-img-top" alt="Image_Formulaire" src="/Images/Formulaire/{{ $formulaire->image }}">
+            <div class="card-body">
+                <h2 class="card-title"> {{ $formulaire->name }} </h2>
+                @if ($formulaire->close_on != NULL)
+                    <p class="card-text"> Disponible jusqu'au {{ $formulaire->close_on }} </p>
+                @else
+                    <p class="card-text"> Pas de <br> date limite </p>
+                @endif
+
+                <div class="auteur">
+                    {{ $formulaire->user->name }}
+                    <img class="img_auteur" src="/avatar/{{$formulaire->user->avatar}}">
+                </div>
+
+            </div>
+        </div>
+
+    @endforeach
+
+        {{ $formulaires->links() }}
+
+</div>
+
 
 @endsection
