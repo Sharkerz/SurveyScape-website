825f804f6fa502bb02a34f235f36bf9a6a681a41
Gandalf0u <leo.auvray@free.fr>
2020-06-11 15:47:45 +0200

    Error404+block+darkTheme

---
+++ src/resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+<link href="{{ asset('css/reponse_success.css') }}" rel="stylesheet">
+
+<div class="card" id="card_success">
+    <h4 class="card-header">Erreur 404</h4>
+    <div class="card-body">
+        <p class="card-text">Cette page n'existe pas</p>
+        <a href="{{ route('accueil') }}" class="btn btn-primary">Retourner à l'accueil</a>
+    </div>
+</div>
+
+@endsection
