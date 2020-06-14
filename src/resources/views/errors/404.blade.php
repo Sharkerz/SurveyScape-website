0ef873a19f9cb366fc2a48eda1c9953f5433c687
Maxence <maxence.lux@supinfo.com>
2020-06-14 11:13:04 +0200

    tri routes

--- src/resources/views/errors/404.blade.php
+++ src/resources/views/errors/404.blade.php
@@ -5,7 +5,7 @@
 <link href="{{ asset('css/reponse_success.css') }}" rel="stylesheet">
 
 <div class="card" id="card_success">
-    <h4 class="card-header">Erreur 404</h4>
+    <h4 class="card-header">Erreur page introuvable</h4>
     <div class="card-body">
         <p class="card-text">Cette page n'existe pas</p>
         <a href="{{ route('home') }}" class="btn btn-primary">Retourner à l'accueil</a>
